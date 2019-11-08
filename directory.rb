3996082a2b7a292e742f04939bd654163d5e7318
Daniel <demartinezgatell@gmail.com>
2019-11-08 23:25:58 +0000

    arranged into methods

--- directory.rb
+++ directory.rb
@@ -15,10 +15,21 @@ students = [
  "Norman Bates"
 ]
 # and then print them
+def print_header
  puts "The students of Villains Academy"
  puts "-------------------------"
- students.each do |student|
-   puts student
+end
+
+def print(names)
+ names.each do |names|
+   puts names
  end
+end
  #Finally we print the total,
- puts "Overall, we have #{students.count} great students"
+ def print_footer(names)
+ puts "Overall, we have #{names.count} great students"
+end
+#nothing happens until we call the methods
+print_header
+print(students)
+print_footer(students)
